0d94b4f973f41b56282b8a7e0373a82574db8a93
Jackson Tong <tongtoan2704@gmail.com>
2016-08-19 23:05:37 +0700

    simplifying the arrow function

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
 
-const store = createStore(todoApp);
+const store = createStore(todoApp, window.devToolsExtension && window.devToolsExtension());
 
 render(
   <Provider store={store}>
