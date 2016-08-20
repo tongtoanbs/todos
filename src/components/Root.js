720b7253a2ce8a938a1842c89c2d1486188c0646
Jackson Tong <tongtoan2704@gmail.com>
2016-08-20 10:19:48 +0700

    adding router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import App from './App';
 import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
 
 const Root = ({store}) => (
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>
   </Provider>
 );
 
