fa7da52470a527f251740d1e99c0aecf715d5873
Jackson Tong <tongtoan2704@gmail.com>
2016-08-21 11:29:21 +0700

    adding debug tool

--- src/configureStore.js
+++ src/configureStore.js
@@ -5,7 +5,11 @@ import throttle from 'lodash/throttle';
 
 const configureStore = () => {
   const persistentData = loadState();
-  const store = createStore(todoApp, persistentData);
+  const store = createStore(
+    todoApp,
+    persistentData,
+    process.env.NODE_ENV !== 'production' ? window.devToolsExtension && window.devToolsExtension() : null
+  );
 
   store.subscribe(throttle(() => {
     saveState({
