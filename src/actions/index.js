672a950fcd69eb3b0fe78e08e3aeb644b48624a1
Jackson Tong <tongtoan2704@gmail.com>
2016-08-26 17:59:52 +0700

    Updating Data on the Server

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,12 +11,13 @@ export const addTodo = (text) => (dispatch) =>
     })
   );
 
-export const toggleTodo = (id) => {
-  return {
-    type: 'TOGGLE_TODO',
-    id,
-  };
-};
+export const toggleTodo = (id) => (dispatch) =>
+  api.toggleTodo(id).then(response =>
+    dispatch({
+      type: 'TOGGLE_TODO_SUCCESS',
+      response: normalize(response, fromSchema.todo)
+    })
+  );
 
 export const fetchTodos = (filter) => (dispatch, getState) => {
   if (getIsFetching(getState(), filter)) {
